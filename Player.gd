4705e919b5fda0a3842888aef305bbd6eb2a4b91
Tony Marklove <tony@tonymarklove.net>
2018-06-22 01:23:47 +0100

    Almost working changable gravity direction

--- Player.gd
+++ Player.gd
@@ -7,27 +7,41 @@ var jump_speed = -1000
 var gravity = 2500
 
 var velocity = Vector2()
+var gravity_direction = Vector2(0, 1)
 
 func get_input():
-	velocity.x = 0
+	var face_direction = gravity_direction.tangent()
+	
+	var x_frac = face_direction.dot(Vector2(1,0))
+	var y_frac = face_direction.dot(Vector2(0,-1))
+	
+	velocity.x *= 1 - x_frac
+	velocity.y *= 1 - y_frac
+	
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_select')
-
+	var gravity_right = Input.is_action_just_pressed('gravity_right')
+	
 	if is_on_floor() and jump:
-		velocity.y = jump_speed
+		velocity += gravity_direction * jump_speed
 	if right:
-		velocity.x += run_speed
+		velocity += face_direction * run_speed
 	if left:
-		velocity.x -= run_speed
-		
-	if abs(velocity.x) > 0:
+		velocity -= face_direction * run_speed
+
+	if x_frac > 0:
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-		
+
+	if gravity_right:
+		gravity_direction = Vector2(1, 0)
+
 func _physics_process(delta):
-	velocity.y += gravity * delta
+	velocity += gravity_direction * gravity * delta
 	get_input()
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	velocity = move_and_slide(velocity, -gravity_direction)
+	
+	$Camera2D.set_rotation(acos(gravity_direction.dot(Vector2(0,1))))
 	
