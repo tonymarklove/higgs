d6f298d8cdb92bbc01a697324c20a5c1fdadd938
Tony Marklove <tony@fluxbit.co.uk>
2018-06-26 18:55:53 +0100

    Camera relative gravity selection

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-export var rotate_to_follow_gravity = false
+export var rotate_to_follow_gravity = true
 export var rotate_camera_with_player = false
+export var player_relative_controls = false
 
 const GRAVITY_DIRECTIONS = [
 	Vector2(0,1),
@@ -32,52 +33,81 @@ func clamp_and_normalize(input, a, b):
 func gravity_vector():
 	return GRAVITY_DIRECTIONS[gravity_direction]
 
-func gravity_select(change):
-	gravity_direction = (gravity_direction + change) % GRAVITY_DIRECTIONS.size()
+func gravity_index_from_offset(offset):
+	return (gravity_direction + offset) % GRAVITY_DIRECTIONS.size()
+
+func gravity_select(direction_index):
+	gravity_direction = direction_index
 	last_gravity_change_time = game_time
 	rotation_at_gravity_change = self.rotation
 
-func get_input():
-	var face_direction = gravity_vector().tangent()
+func gravity_change_from_input():
+	if Input.is_action_just_pressed('gravity_right'):
+		return 1
 
-	var x_frac = face_direction.dot(Vector2(1,0))
-	var y_frac = face_direction.dot(Vector2(0,-1))
+	if Input.is_action_just_pressed('gravity_left'):
+		return -1
+	
+	if Input.is_action_just_pressed('gravity_up'):
+		return 2
+
+	return 0
+
+func rotating_camera_gravity_select():
+	var change = gravity_change_from_input()
+	
+	if change != 0:
+		gravity_select(gravity_index_from_offset(change))
+
+func fixed_camera_gravity_select():
+	if Input.is_action_just_pressed('gravity_right'):
+		gravity_select(1)
+
+	if Input.is_action_just_pressed('gravity_left'):
+		gravity_select(3)
+	
+	if Input.is_action_just_pressed('gravity_up'):
+		gravity_select(2)
+
+	if Input.is_action_just_pressed('gravity_down'):
+		gravity_select(0)
+
+func get_input():
+	var player_right_direction = gravity_vector().tangent()
+
+	var x_frac = player_right_direction.dot(Vector2(1,0))
+	var y_frac = player_right_direction.dot(Vector2(0,-1))
 
 	velocity.x *= 1 - abs(x_frac)
 	velocity.y *= 1 - abs(y_frac)
 
 	var right = Input.is_action_pressed('ui_right')
 	var left = Input.is_action_pressed('ui_left')
+	var up = Input.is_action_pressed('ui_up')
+	var down = Input.is_action_pressed('ui_down')
 	var jump = Input.is_action_just_pressed('ui_select')
-	var gravity_right = Input.is_action_just_pressed('gravity_right')
-	var gravity_left = Input.is_action_just_pressed('gravity_left')
-	var gravity_flip = Input.is_action_just_pressed('gravity_flip')
 
 	if is_on_floor() and jump:
 		velocity += gravity_vector() * jump_speed
 	if right:
 		$AnimatedSprite.flip_h = false
-		velocity += face_direction * run_speed
+		velocity += player_right_direction * run_speed
 	if left:
 		$AnimatedSprite.flip_h = true
-		velocity -= face_direction * run_speed
+		velocity -= player_right_direction * run_speed
 
-	var walk_movement = velocity.dot(face_direction)
+	var walking = left || right
 
-	if abs(walk_movement) > 0.1:
+	if walking:
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
 		$AnimatedSprite.frame = 0
 
-	if gravity_right:
-		gravity_select(1)
-
-	if gravity_left:
-		gravity_select(-1)
-
-	if gravity_flip:
-		gravity_select(2)
+	if player_relative_controls || rotate_camera_with_player:
+		rotating_camera_gravity_select()
+	else:
+		fixed_camera_gravity_select()
 
 func rotate_to_gravity():
 	var desired_rotation = -gravity_vector().angle_to(Vector2(0,1))
